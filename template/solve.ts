745d5913b97f99a0deb1c1a6867863d870c385c5
Gareth Knowles <gareth@mettle.co.uk>
2020-12-02 14:07:01 +0000

    Better result handling

--- template/solve.ts
+++ template/solve.ts
@@ -2,13 +2,16 @@ import { solution } from './solution'
 import fs from 'fs'
 import path from 'path'
 
+const shouldConsoleResult = process.argv.find((arg) => arg === '--result')
+
 export const solve = async () => {
   const inputFile = fs
     .readFileSync(path.join(__dirname, './input.txt'), 'utf-8')
     .split(/\r?\n/)
     .filter((value) => value !== '')
 
-  return await solution(inputFile)
+  const result = await solution(inputFile)
+  shouldConsoleResult && console.log(result)
 }
 
 solve()
