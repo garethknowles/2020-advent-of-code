bb94a53e7b4152d397e3a78e24c5702e6bcf64fc
Gareth Knowles <gareth@mettle.co.uk>
2020-12-09 11:59:14 +0000

    Day 9 b

--- 2020-9/solution.test.ts
+++ 2020-9/solution.test.ts
@@ -11,3 +11,15 @@ test('solution', async () => {
   const result = await solution({ fileName: 'input.txt', preambleSize: 25 })
   expect(result).toEqual(expected)
 })
+
+test('example1b', async () => {
+  const expected = 62
+  const result = await solution({ fileName: 'example1.txt', preambleSize: 5, solutionB: true })
+  expect(result).toEqual(expected)
+})
+
+test('solutionb', async () => {
+  const expected = 23761694
+  const result = await solution({ fileName: 'input.txt', preambleSize: 25, solutionB: true })
+  expect(result).toEqual(expected)
+})
--- 2020-9/solution.ts
+++ 2020-9/solution.ts
@@ -1,13 +1,24 @@
 import { handleInputs } from './inputHelpers'
 
-export const solution = async ({ fileName, input, preambleSize }: { fileName?: string; input?: string[]; preambleSize: number }) => {
-  const data: string[] = await handleInputs({ fileName, input })
+const setThatSums = (data: number[], sumResult: number): number[] => {
+  console.log(sumResult)
+  for (let setSize = 2; setSize < data.length; setSize++) {
+    for (let i = 0; i < data.length - setSize; i++) {
+      const set = data.slice(i, i + setSize)
+      const setTotal = set.reduce((prev, cur) => prev + cur, 0)
+      if (setTotal === sumResult) {
+        console.log(set)
+        return set
+      }
+    }
+  }
+  return []
+}
 
-  const dataAsNumbers: number[] = data.map((d) => Number(d))
-
-  for (let i = preambleSize; i < dataAsNumbers.length; i++) {
-    const currentNumber = dataAsNumbers[i]
-    const previousNumbers = dataAsNumbers.slice(i - preambleSize, i)
+const findInvalidNumber = (data: number[], preambleSize: number) => {
+  for (let i = preambleSize; i < data.length; i++) {
+    const currentNumber = data[i]
+    const previousNumbers = data.slice(i - preambleSize, i)
 
     let foundSum = false
     previousNumbers.forEach((a, ai) =>
@@ -25,6 +36,24 @@ export const solution = async ({ fileName, input, preambleSize }: { fileName?: s
       return currentNumber
     }
   }
+}
 
-  return undefined
+const findEncryptionWeakness = (data: number[], invalidNumber: number) => {
+  ;[3, 21, 11].sort() //?
+  ;[3, 21, 11].sort((a, b) => (a < b ? -1 : 1)) //?
+
+  const set = setThatSums(data, invalidNumber).sort((a, b) => (a < b ? -1 : 1)) //?
+  return set[0] + set.reverse()[0]
+}
+
+export const solution = async ({ fileName, input, preambleSize, solutionB = false }: { fileName?: string; input?: string[]; preambleSize?: number; solutionB?: boolean }) => {
+  const data: string[] = await handleInputs({ fileName, input })
+
+  const dataAsNumbers: number[] = data.map((d) => Number(d))
+  const invalidNumber = findInvalidNumber(dataAsNumbers, preambleSize)
+  if (!solutionB) {
+    return invalidNumber
+  }
+
+  return findEncryptionWeakness(dataAsNumbers, invalidNumber)
 }
